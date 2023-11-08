ef0c49b3b675af06fcfbf4741676180c29b597b0
Klissman Idarraga <klesteban0.3@gmail.com>
2023-11-08 01:46:01 -0500

    set api token and secret Keys

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,8 +99,6 @@ dependencies {
 
     //Dagger-Hilt
     implementation ("com.google.dagger:hilt-android:2.48.1")
-    testImplementation("junit:junit:4.12")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     kapt ("com.google.dagger:hilt-android-compiler:2.48.1")
     kapt ("androidx.hilt:hilt-compiler:1.1.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
